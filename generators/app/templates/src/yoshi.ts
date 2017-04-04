273ad695da62a0e71d721c92584fddbceee420fa
Ha <typ0@naver.com>
2017-04-04 09:45:18 +0900

    extend response type

--- generators/app/templates/src/yoshi.ts
+++ generators/app/templates/src/yoshi.ts
@@ -1,2 +1,15 @@
-export { MyResponse as Response } from './responses';
-export { Request, NextFunction, Router } from 'express';
+import { MyResponse } from './responses';
+import { Request, NextFunction, Router } from 'express';
+
+interface MyRequest extends Request {
+    user: {
+        id: string;
+    };
+};
+
+export {
+    MyResponse as Response,
+    MyRequest as Request,
+    NextFunction,
+    Router,
+};
